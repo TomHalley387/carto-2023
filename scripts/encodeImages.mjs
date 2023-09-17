b611d403c0c6fbe4a2522bd6b828438bc144af80
felix <you@example.com>
2023-09-17 18:53:32 -0400

    Sun 17 Sep 2023 18:53:32 EDT

--- scripts/encodeImages.mjs
+++ scripts/encodeImages.mjs
@@ -192,7 +192,7 @@ async function start() {
   //const allImages = scanImages(path.join(import.meta.url, "../../concepts"));
   clipImages(allImages);
 
-  await encodeTags();
+  // await encodeTags();
 
   clipByUrl(
     "https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Cat_August_2010-4.jpg/362px-Cat_August_2010-4.jpg"
